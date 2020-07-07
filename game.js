0db21f56f40f8a6c77d06b8a8b35a7bc71f1d94d
arianna choza <unachoza@gmail.com>
2020-07-07 16:13:03 -0400

    rending window frames at speed desired

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
+let lastRenderTime = 0;
 const SNAKE_SPEED = 1;
-
-// const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-// if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
 const main = (currentTime) => {
   window.requestAnimationFrame(main);
-  console.log(currentTime);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  console.log('render');
+  lastRenderTime = currentTime;
 };
-// window.requestAnimationFrame(main);
+
+window.requestAnimationFrame(main);
